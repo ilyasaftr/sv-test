996009e336b8340d4538599185fc5c79669103b2
ilyasafr <ilyasarecord@gmail.com>
2023-12-18 13:53:25 +0700

    Update IP Validation

--- app/Http/Controllers/PingController.php
+++ app/Http/Controllers/PingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Process\Pipe;
 use Illuminate\Support\Facades\Process;
+use Throwable;
 
 class PingController extends Controller
 {
@@ -12,14 +13,35 @@ class PingController extends Controller
     {
         // get query ip address
         $ip = request()->query('ip');
-        $result = Process::run("/sbin/ping -c 4 $ip");
-        $output = $result->output();
-        $output = explode("/", $output);
-        $output = explode(" ", $output[3]);
-        $averageMs = $output[2];
-        return response()->json([
-            'ip' => $ip,
-            'averageMs' => round($averageMs, 2)
-        ]);
+        if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+            return response()->json([
+                'errors' => [
+                    'ip' => [
+                        'The ip field must be a valid IPv4 address.'
+                    ]
+                ]
+            ], 400);
+        }
+
+        try {
+            $result = Process::run("/sbin/ping -c 4 $ip");
+            $output = $result->output();
+            $output = explode("/", $output);
+            $output = explode(" ", $output[3]);
+            $averageMs = $output[2];
+
+            return response()->json([
+                'data' => [
+                    'ip' => $ip,
+                    'average_ms' => $averageMs
+                ],
+            ]);
+        } catch (Throwable $th) {
+            return response()->json([
+                'errors' => [
+                    'message' => $th->getMessage()
+                ]
+            ], 400);
+        }
     }
 }
